f781713d4a098c45c472ebd8c00dd0fdda258bd7
bebrochkas <popyvanovd@bk.ru>
2025-02-19 22:18:36 +0500

    Добавлен тег романтика + подключен мультитег

--- ai/nlp_learning.py
+++ ai/nlp_learning.py
@@ -6,7 +6,11 @@ from spacy.compat import pickle
 def learn_model():
     data = {''.join(list(i)[:-4]):open(f'{os.getcwd()}\\tags_data\\{i}', 'r', encoding='UTF-8').read().split('\n')[1:] for i in os.listdir(f'{os.getcwd()}\\tags_data')}
 
-    nlp = ClassyClassifier(data=data)
+    nlp = ClassyClassifier(
+        data= data,
+        model= "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2",
+        multi_label= True,
+    )
 
     with open(f'{os.getcwd()}\\models\\tagger.pkl', 'wb') as file:
        pickle.dump(nlp, file)

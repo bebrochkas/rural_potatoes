dc763f3534736eaa8aa4e4f64b4a7d1d4086a0db
somuthink <qould12@gmail.com>
2025-02-22 23:54:00 +0500

    missed frontend parts

---
+++ frontend/src/interfaces/interfaces.ts
@@ -0,0 +1,17 @@
+export interface Tag {
+    id: number;
+    name: string;
+    hex: string;
+    type: string;
+}
+
+export interface Film {
+    id: number;
+    title: string;
+    description: string;
+    posterPreUrl: string;
+    posterUrl: string;
+    backdropUrl: string;
+    rate: number;
+    tags: Tag[];
+}
